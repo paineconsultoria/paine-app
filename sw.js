ccbeb0d18f62c85225ca3ea2a16e0a96707c1552
paineconsultoria <paineconsultoria@gmail.com>
2026-07-30 18:41:15 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'paine-v1';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Rede primeiro (pra sempre pegar a versão mais nova); cache só como reserva offline
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
